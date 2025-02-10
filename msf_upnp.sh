5c7cb48ec779d05075126a6ab4d6c1a455e1a57a
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2025-02-10 15:16:09 +0100

    Update msf_upnp.sh

--- msf_upnp.sh
+++ msf_upnp.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+BLEU='\e[34m'
+ROUGE='\033[0;31m'
+VERT='\033[0;32m'
+GRIS='\033[0;37m'
+JAUNE='\e[0;33m'
+RESET='\033[0m'
+BLANC='\033[1;37m'
+
 msf_upnp () {
 
     echo "Port defaut: 1900,5000-5001,2869"
-    read -p "Entrer une ip: " IP
-    read -p "Entrer le port UPNP: " PORT
+    
+    echo -ne "${BLEU}[i]${RESET} ${BLANC}IP:${RESET} "
+    read  IP
+    
+    echo -ne "${BLEU}[i]${RESET} ${BLANC}Port UPNP:${RESET} " 
+    read Port
     
     msfconsole -q -x "use auxiliary/scanner/upnp/ssdp_msearch; set RHOSTS ${IP}; set RPORT ${PORT};set verbose true; run; exit"
 }
